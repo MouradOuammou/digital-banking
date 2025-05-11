450c68d4c1fe1ad11d2432320e81dd02a41299c4
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-11 12:35:31 +0100

    cote Repistory ajouter searshCustomer

--- src/main/java/ouammou/digital_banking/repositories/BankAccountRepository.java
+++ src/main/java/ouammou/digital_banking/repositories/BankAccountRepository.java
@@ -1,7 +1,14 @@
 package ouammou.digital_banking.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ouammou.digital_banking.entites.BankAccount;
+import ouammou.digital_banking.entites.Customer;
+
+import java.util.List;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+    @Query("select c from Customer c where c.name like :kw")
+    List<Customer> searchCustomer(@Param("kw") String keyword);
 }
