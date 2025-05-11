4f8e19714a651ed03ff40a226c983107b6f8993c
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-11 12:27:27 +0100

    cote RestApi de BankAccount ajouter gethitstory et listAccounts

--- src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
+++ src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ouammou.digital_banking.dtos.AccountOperationDTO;
 import ouammou.digital_banking.dtos.BankAccountDTO;
 import ouammou.digital_banking.exceptions.BankAccountNotFoundException;
 import ouammou.digital_banking.services.BankAccountService;
+
+import java.util.List;
 
 @RestController
 public class BankAccountRestAPI {
@@ -19,5 +22,13 @@ public class BankAccountRestAPI {
     @GetMapping("/accounts/{accountId}")
     public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
         return bankAccountService.getBankAccount(accountId);
+    }
+    @GetMapping("/accounts")
+    public List<BankAccountDTO> listAccounts(){
+        return bankAccountService.bankAccountList();
+    }
+    @GetMapping("/accounts/{accountId}/operations")
+    public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
+        return bankAccountService.accountHistory(accountId);
     }
 }
