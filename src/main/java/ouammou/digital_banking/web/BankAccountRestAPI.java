8a596990a8d1d1a352c56ed1a38c039f1a411dfd
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-11 12:26:54 +0100

    cote RestApi de BankAccount ajouter get

--- src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
+++ src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
@@ -1,8 +1,23 @@
 package ouammou.digital_banking.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ouammou.digital_banking.dtos.BankAccountDTO;
+import ouammou.digital_banking.exceptions.BankAccountNotFoundException;
+import ouammou.digital_banking.services.BankAccountService;
 
 @RestController
 public class BankAccountRestAPI {
+    private BankAccountService bankAccountService;
+
+    public BankAccountRestAPI(BankAccountService bankAccountService) {
+        this.bankAccountService = bankAccountService;
+    }
+
+    @GetMapping("/accounts/{accountId}")
+    public BankAccountDTO getBankAccount(@PathVariable String accountId) throws BankAccountNotFoundException {
+        return bankAccountService.getBankAccount(accountId);
+    }
 }
