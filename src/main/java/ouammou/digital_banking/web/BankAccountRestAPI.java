abbba9eddbb6ff810d2755744599ddfbd66a4891
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-11 12:29:00 +0100

    ajouter 2 methode post et get mapping  debit avec debitdto et  accounthistory

--- src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
+++ src/main/java/ouammou/digital_banking/web/BankAccountRestAPI.java
@@ -1,11 +1,11 @@
 package ouammou.digital_banking.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ouammou.digital_banking.dtos.AccountHistoryDTO;
 import ouammou.digital_banking.dtos.AccountOperationDTO;
 import ouammou.digital_banking.dtos.BankAccountDTO;
+import ouammou.digital_banking.dtos.DebitDTO;
+import ouammou.digital_banking.exceptions.BalanceNotSufficientException;
 import ouammou.digital_banking.exceptions.BankAccountNotFoundException;
 import ouammou.digital_banking.services.BankAccountService;
 
@@ -30,5 +30,17 @@ public class BankAccountRestAPI {
     @GetMapping("/accounts/{accountId}/operations")
     public List<AccountOperationDTO> getHistory(@PathVariable String accountId){
         return bankAccountService.accountHistory(accountId);
+    }
+    @GetMapping("/accounts/{accountId}/pageOperations")
+    public AccountHistoryDTO getAccountHistory(
+            @PathVariable String accountId,
+            @RequestParam(name="page",defaultValue = "0") int page,
+            @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
+        return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
     }
 }
