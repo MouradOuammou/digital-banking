50fcbce8fa30fbdc61aadc507f167fdbdbaff68a
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-11 12:23:11 +0100

    fixation des problemes

--- src/main/java/ouammou/digital_banking/services/BankAccountService.java
+++ src/main/java/ouammou/digital_banking/services/BankAccountService.java
@@ -18,6 +18,7 @@ public interface BankAccountService {
     void credit(String accountId, double amount, String description) throws BankAccountNotFoundException;
     void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
     List<BankAccountDTO> bankAccountList();
+
     CustomerDTO getCustomer(Long customerId) throws CustomerNotFoundException;
     CustomerDTO updateCustomer(CustomerDTO customerDTO);
     void deleteCustomer(Long customerId);
