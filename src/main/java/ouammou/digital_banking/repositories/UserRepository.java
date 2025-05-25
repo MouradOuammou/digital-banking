92251311f6e4d7ecdd3446ba51bb84ac72d26478
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-25 14:30:45 +0100

    creation de repository user

---
+++ src/main/java/ouammou/digital_banking/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package ouammou.digital_banking.repositories;
+
+import ouammou.digital_banking.entites.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
