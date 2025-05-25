9a44a1031b24228810a9b670c3d3be56b410684b
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-25 16:55:33 +0100

    fixer le pro

--- src/main/java/ouammou/digital_banking/security/SecurityController.java
+++ src/main/java/ouammou/digital_banking/security/SecurityController.java
@@ -1,19 +1,17 @@
 package ouammou.digital_banking.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ouammou.digital_banking.services.UserDetailsServiceImpl;
 
-import javax.naming.AuthenticationException;
+import java.util.Map;
 
 @RestController
-
 public class SecurityController {
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -24,23 +22,25 @@ public class SecurityController {
     private UserDetailsServiceImpl userDetailsService;
 
     @PostMapping("/authenticate")
-    public String createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
-        );
+    public Map<String, String> createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("Identifiants incorrects", e);
+        }
 
-        final var userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
+        var userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+        String jwt = jwtUtil.generateToken(userDetails.getUsername());
 
-        return jwt;
+        return Map.of("token", jwt);
     }
 }
 
 class AuthRequest {
     private String username;
     private String password;
-
-    // getters et setters
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
 
