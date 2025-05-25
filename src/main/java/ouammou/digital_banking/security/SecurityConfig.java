a35e8b6696b799f463482b942e8cb4c41c84e5af
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-25 14:43:14 +0100

    comfigurer security

--- src/main/java/ouammou/digital_banking/security/SecurityConfig.java
+++ src/main/java/ouammou/digital_banking/security/SecurityConfig.java
@@ -1,9 +1,43 @@
-    package ouammou.digital_banking.security;
+package ouammou.digital_banking.security;
 
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import ouammou.digital_banking.services.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig {
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl uds) {
+        this.userDetailsService = uds;
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/customers/**").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
